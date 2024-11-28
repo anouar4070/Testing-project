2ceeabea1abdb78f56720ff1aefde5d70c2f6910
anouar garbaa <g.anouar@yahoo.com>
2024-11-28 18:27:28 +0100

    working with Mocks

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -2,9 +2,13 @@ import { render, screen } from "@testing-library/react";
 import Async from "./Async";
 
 describe("Async component", () => {
-  test("renders posts if request succeeds", async() => {
+  test("renders posts if request succeeds", async () => {
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [{ id: "p1", title: "First post" }],
+    });
     render(<Async />);
-//getAllByRole() returns Promise
+    //getAllByRole() returns Promise
     const listItemElements = await screen.findAllByRole("listitem");
     //we don't use getAllByRole() because it returns the first render where [] is empty
     //const listItemElements = screen.getAllByRole("listitem");
